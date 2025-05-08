9f6d0cb464579a9daed0d93015796c2da9460b6b
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-08 14:50:41 +0200

    moved the jwt code into the post/login

--- routes/auth_login.js
+++ routes/auth_login.js
@@ -5,9 +5,9 @@ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
 router.post("/login", async (req, res) => {
+  const JWT_SECRET = process.env.JWT_SECRET;
+
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
