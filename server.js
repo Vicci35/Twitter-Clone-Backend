cc7c175add0a16f5ef6dc662294f3b03e7485869
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-08 13:17:39 +0200

    added dotenv to the server file

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 
 import app from "./app.js";
@@ -6,16 +9,16 @@ import mongoKey from "./config/db.js";
 const PORT = 3000;
 
 async function start() {
-    try {
-        await mongoose.connect(mongoKey);
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`Server started: http://localhost${PORT}`)
-        })
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(mongoKey);
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server started: http://localhost${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 start();
