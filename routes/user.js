f926cbfc13eb6c723b76b9abc86a38eeb3b3f7c3
Oliver Eriksson <oeriksson854@gmail.com>
2025-05-12 12:39:26 +0200

    Added get:id and get user/:id

--- routes/user.js
+++ routes/user.js
@@ -46,4 +46,18 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    //"-password" ensures that requests do not include the password
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message   });
+  }
+})
+
 export default router;
