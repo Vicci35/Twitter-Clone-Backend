faa60bc96dbca4c4de248a252af69224eb4b0f21
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-05-14 00:19:46 +0200

    refactor: remove debug console statements

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,8 +4,6 @@ import authenticateToken from "./middleware/authToken.js";
 const dashRouter = express.Router();
 
 dashRouter.get("/", authenticateToken, (req, res) => {
-  console.log(req.user);
-
   res.json({
     message: "Logged in",
     user: req.user,
