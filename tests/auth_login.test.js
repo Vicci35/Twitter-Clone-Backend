b1bd59b2d406272c0bbe7c7cae8c960d2bc179c3
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-13 20:11:41 +0200

    added vitest and imported dotenv

--- tests/auth_login.test.js
+++ tests/auth_login.test.js
@@ -1,16 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import request from "supertest";
 import app from "../app.js";
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import { getHashedPassword } from "../services/auth.js";
+import { describe, it, beforeAll, afterAll, expect } from "vitest";
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI);
-
   await User.deleteMany({ email: "test@example.com" });
 
   const hashedPassword = await getHashedPassword("lösenord123");
-
   await User.create({
     email: "test@example.com",
     password: hashedPassword,
@@ -23,22 +25,24 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test("logga in korrekt användare och retunera token", async () => {
-  const res = await request(app)
-    .post("/api/login")
-    .send({ email: "test@example.com", password: "lösenord123" });
+describe("Login tests", () => {
+  it("ska logga in korrekt användare och returnera token", async () => {
+    const res = await request(app)
+      .post("/api/login")
+      .send({ email: "test@example.com", password: "lösenord123" });
 
-  expect(res.statusCode).toBe(200);
-  expect(res.body.token).toBeDefined();
-  expect(res.body.user.email).toBe("test@example.com");
-}, 10000);
-
-test("ska returnera 401 vid felaktigt lösenord", async () => {
-  const res = await request(app).post("/api/login").send({
-    email: "test@example.com",
-    password: "felLösen",
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+    expect(res.body.user.email).toBe("test@example.com");
   });
 
-  expect(res.statusCode).toBe(401);
-  expect(res.body.message).toBe("Fel lösenord.");
+  it("ska returnera 401 vid felaktigt lösenord", async () => {
+    const res = await request(app).post("/api/login").send({
+      email: "test@example.com",
+      password: "felLösen",
+    });
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.message).toBe("Fel lösenord.");
+  });
 });
