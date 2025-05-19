28ee900c2929061ffb94183dfdcfc7140149e194
Victoria Tobisson Lind <toja.1999@hotmail.com>
2025-05-19 11:02:26 +0200

    took away timestamps

--- models/Follow.js
+++ models/Follow.js
@@ -1,20 +1,17 @@
 import mongoose from "mongoose";
 
-const followSchema = new mongoose.Schema(
-  {
-    followerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User", //Den som följer
-      required: true,
-    },
-    targetUserId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User", //dem som blir följd
-      required: true,
-    },
+const followSchema = new mongoose.Schema({
+  followerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", //Den som följer
+    required: true,
   },
-  { timestamps: true }
-);
+  targetUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", //dem som blir följd
+    required: true,
+  },
+});
 
 const Follow = mongoose.model("Follow", followSchema);
 
